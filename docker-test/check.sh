b6ea1f74acca400709b5f3aca1c778a60e8b9bae
Tim 'mithro' Ansell <mithro@mithis.com>
2014-12-27 23:39:53 +1030

    Change to a directory you can write too.

--- docker-test/check.sh
+++ docker-test/check.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+cd /tmp
+
 # Check that I'm running under trusty
 if [ $(lsb_release -c -s) != "trusty" ]; then
   echo "Not running under trusty!"
@@ -21,11 +23,14 @@ sudo /bin/true
 # Check I can apt-get install
 sudo apt-get -y --force-yes install vim
 
+umask
+
 # Check I can compile something
 cat > test.c <<EOF
 int main(void) {
   return 0;
 }
 EOF
+ls -l test.c
 gcc test.c -o test
 ./test
